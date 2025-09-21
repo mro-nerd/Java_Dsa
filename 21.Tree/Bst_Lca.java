840693e73655758bb8f4586b23f0fe72f22b1804
mro-nerd <adi1492mi@gmail.com>
2025-09-21 22:46:34 +0530

    m3

--- 21.Tree/Bst_Lca.java
+++ 21.Tree/Bst_Lca.java
@@ -12,7 +12,7 @@ Example 2:
 
 Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
 Output: 2
-Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition. */
+Explanation: The LCA of nodes 2 and 4 is 2. */
 class TreeNode {
     int val;
     TreeNode left, right;
