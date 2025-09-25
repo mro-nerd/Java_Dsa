abc6843fd89a8f98d3394f8044ab915030cbd49c
mro-nerd <adi1492mi@gmail.com>
2025-09-25 23:01:15 +0530

    stream

--- 21.Tree/validBst.java
+++ 21.Tree/validBst.java
@@ -14,7 +14,9 @@ Example 2:
 
 Input: root = [5,1,4,null,null,3,6]
 Output: false
-Explanation: The root node's value is 5 but its right child's value is 4. */
+Explanation: The root node's value is 5 but its right child's value is 4. 
+
+*/
 class TreeNode {
     int val;
     TreeNode left, right;
