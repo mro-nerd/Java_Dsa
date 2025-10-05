ee19ac48007d0bd362c32457e409735003d4c163
mro-nerd <adi1492mi@gmail.com>
2025-10-05 23:42:47 +0530

    hw

--- 21.Tree/validBst.java
+++ 21.Tree/validBst.java
@@ -13,8 +13,7 @@ Output: true
 Example 2:
 
 Input: root = [5,1,4,null,null,3,6]
-Output: false
-Explanation: The root node's value is 5 but its right child's value is 4. 
+Output: false 
 
 */
 class TreeNode {
