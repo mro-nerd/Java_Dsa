e6a312ded1520dca277ae2c54a7c81ded9a981af
mro-nerd <adi1492mi@gmail.com>
2025-10-25 23:51:09 +0530

    row

--- 19.DP/uniquePaths.java
+++ 19.DP/uniquePaths.java
@@ -6,7 +6,9 @@ The testcases are generated so that the answer will be less than or equal to 2 *
 Example 1:
 
 Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+
 Output: 2
+
 Explanation: There is one obstacle in the middle of the 3x3 grid above.
 There are two ways to reach the bottom-right corner:
 1. Right -> Right -> Down -> Down
