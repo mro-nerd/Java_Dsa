9c468b2aa9e3ea5a23e13099c16a09a8b35c2e5b
mro-nerd <adi1492mi@gmail.com>
2025-10-26 23:20:18 +0530

    next

--- 19.DP/uniquePaths.java
+++ 19.DP/uniquePaths.java
@@ -11,6 +11,7 @@ Output: 2
 
 Explanation: There is one obstacle in the middle of the 3x3 grid above.
 There are two ways to reach the bottom-right corner:
+
 1. Right -> Right -> Down -> Down
 2. Down -> Down -> Right -> Right 
 
