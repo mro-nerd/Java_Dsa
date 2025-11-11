1e34030493e947a69cbaedc49bc73f83dfea55df
mro-nerd <adi1492mi@gmail.com>
2025-11-11 23:32:47 +0530

    //

--- 15.Hashmap/randomSet.java
+++ 15.Hashmap/randomSet.java
@@ -6,7 +6,6 @@
 *int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
 
 You must implement the functions of the class such that each function works in average O(1) time complexity.
-
 Example 1:
 
 Input
