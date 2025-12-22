330d3a6f8abcf97467a499236e3f316f57bf19e3
mro-nerd <adi1492mi@gmail.com>
2025-12-22 21:10:35 +0530

    changes

--- 19.DP/coinChange.java
+++ 19.DP/coinChange.java
@@ -8,7 +8,8 @@ Input:
 amount = 5, coins = [1,2,5]
 Output: 
 4
-Explanation: there are four ways to make up the amount:
+Explanation: 
+There are four ways to make up the amount:
 5=5
 5=2+2+1
 5=2+1+1+1
