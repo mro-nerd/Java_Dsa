34b4ecf6cef39ab1c672c8bf97a2db39e97e3d77
mro-nerd <adi1492mi@gmail.com>
2026-02-11 22:37:35 +0530

    indent

--- 20.Probability/archer.java
+++ 20.Probability/archer.java
@@ -15,12 +15,10 @@ import java.util.*;
 public final class archer {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
         int d = sc.nextInt();
-
         double Wa = a/(double)b, Wb = c/(double)d;
         double La = 1.0 - Wa, Lb = 1.0 - Wb;
 
