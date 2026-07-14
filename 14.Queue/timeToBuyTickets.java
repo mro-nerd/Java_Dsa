81c4b023ba1c2586ef3b64445ad6536af1129319
mro-nerd <adi1492mi@gmail.com>
2026-07-14 20:55:05 +0530

    ch2

--- 14.Queue/timeToBuyTickets.java
+++ 14.Queue/timeToBuyTickets.java
@@ -6,8 +6,6 @@ Each person takes exactly 1 second to buy a ticket. A person can only buy 1 tick
 
 Return the time taken for the person initially at position k (0-indexed) to finish buying tickets.
 
- 
-
 Example 1:
 
 Input: tickets = [2,3,2], k = 2
@@ -22,7 +20,8 @@ Continuing this process, the queue becomes [2,1,2] at 2 seconds.
 Continuing this process, the queue becomes [1,2,1] at 3 seconds.
 Continuing this process, the queue becomes [2,1] at 4 seconds. Note: the person at the front left the queue.
 Continuing this process, the queue becomes [1,1] at 5 seconds.
-Continuing this process, the queue becomes [1] at 6 seconds. The kth person has bought all their tickets, so return 6. */
+Continuing this process, the queue becomes [1] at 6 seconds. The kth person has bought all their tickets, so return 6.
+*/
 
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
