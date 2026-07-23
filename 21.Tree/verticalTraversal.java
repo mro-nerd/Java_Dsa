a39126399a45f7bbb790bc65564edbae18594633
mro-nerd <adi1492mi@gmail.com>
2026-07-23 15:41:59 +0530

    m1

--- 21.Tree/verticalTraversal.java
+++ 21.Tree/verticalTraversal.java
@@ -8,8 +8,10 @@ Return the vertical order traversal of the binary tree.
 
 Example 1:
 
-Input: root = [3,9,20,null,null,15,7]
-Output: [[9],[3,15],[20],[7]]
+Input:
+root = [3,9,20,null,null,15,7]
+Output: 
+[[9],[3,15],[20],[7]]
 Explanation:
 Column -1: Only node 9 is in this column.
 Column 0: Nodes 3 and 15 are in this column in that order from top to bottom.
@@ -18,8 +20,10 @@ Column 2: Only node 7 is in this column.
 
 Example 2:
 
-Input: root = [1,2,3,4,5,6,7]
-Output: [[4],[2],[1,5,6],[3],[7]]
+Input: 
+root = [1,2,3,4,5,6,7]
+Output: 
+[[4],[2],[1,5,6],[3],[7]]
 Explanation:
 Column -2: Only node 4 is in this column.
 Column -1: Only node 2 is in this column.
@@ -27,7 +31,8 @@ Column 0: Nodes 1, 5, and 6 are in this column.
           1 is at the top, so it comes first.
           5 and 6 are at the same position (2, 0), so we order them by their value, 5 before 6.
 Column 1: Only node 3 is in this column.
-Column 2: Only node 7 is in this column.*/
+Column 2: Only node 7 is in this column.
+*/
 
 import java.util.*;
 
